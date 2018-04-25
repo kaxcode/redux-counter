64961fe2909336690d66b8ce87dded01485e5767
Kenia <keniavalladarez@gmail.com>
2018-04-25 11:38:03 -0400

    Implement a decrement action.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function reducer(state, action) {
   if (action.type === "INCREMENT") {
     return state + 1;
+  } else if (action.type === "DECREMENT") {
+    return state - 1;
   } else {
     return state;
   }
@@ -16,3 +18,9 @@ const unknownAction = { type: "UNKNOWN" };
 
 console.log(reducer(5, unknownAction));
 console.log(reducer(8, unknownAction));
+
+const decrementAction = { type: "DECREMENT" };
+
+console.log(reducer(10, decrementAction));
+console.log(reducer(9, decrementAction));
+console.log(reducer(5, decrementAction));
