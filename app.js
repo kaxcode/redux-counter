64f08e2cc55bf4bd31c5f648427ca3c85a05b24b
Kenia <keniavalladarez@gmail.com>
2018-04-25 11:51:29 -0400

    Create our own version of the createStore() func.

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-function reducer(state, action) {
-  if (action.type === "INCREMENT") {
-    return state + action.amount;
-  } else if (action.type === "DECREMENT") {
-    return state - action.amount;
-  } else {
-    return state;
-  }
+function createStore(reducer) {
+  let state = 0;
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+  };
+
+  return {
+    getState,
+    dispatch
+  };
 }
-
-const incrememtAction = { type: "INCREMENT", amount: 5 };
-
-console.log(reducer(0, incrememtAction));
-console.log(reducer(1, incrememtAction));
-
-const decrementAction = { type: "DECREMENT", amount: 11 };
-
-console.log(reducer(100, decrementAction));
